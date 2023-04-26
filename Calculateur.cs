517123aeff439dfc3b2164679ce196be70a840f0
Owen Le Nadant <2130509@etu.cegepjonquiere.ca>
2023-04-26 16:16:01 -0400

    Calculateur

---
+++ Calculateur.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculateur
+{
+    internal class Calculateur
+    {
+        private int x;
+        private int y;
+
+        public int Diviser(int x, int y)
+        {
+            int calcul = 0;
+            calcul = x / y;
+
+            return calcul;
+        }
+
+        public int Multiplier(int x, int y)
+        {
+            int calcul = 0;
+            calcul = x * y;
+
+            return calcul;
+        }
+
+        public int Additionner(int x, int y)
+        {
+            int calcul = 0;
+            calcul = x + y;
+            return calcul;
+        }
+
+        public int Soustraction(int x, int y)
+        {
+            int calcul = 0;
+            calcul = x - y;
+            return calcul;
+        }
+
+        public double Exposant(int x, int y)
+        {
+            double calcul = 0;
+            calcul = Math.Pow(x,y);
+            return calcul;
+        }
+
+        public double Logarithmes(int x, int y)
+        {
+            double calcul = 0;
+            calcul = Math.Log(x,y);
+            return calcul;
+        }
+    }
+}
